9b945b3e173fb2a525f00aabd4dd672048361815
jpierron <pieron.jag@gmail.com>
2015-03-06 17:48:32 +0100

    TEST

---
+++ app/src/main/java/timager/jpierron/timager/TimagerHome.java
@@ -0,0 +1,16 @@
+package timager.jpierron.timager;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by jpierron on 24/02/2015.
+ */
+public class TimagerHome extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timager_home);
+    }
+}
